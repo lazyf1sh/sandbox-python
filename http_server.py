e3036d58e6425efedee35eef1c5950f601168ebd
lazyf1sh <f1sh@pm.me>
2018-03-27 15:11:43 +0300

    initial

---
+++ http_server.py
@@ -0,0 +1,51 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+# HTTPRequestHandler class
+class testHttpServerRequestHandler(BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+    def do_POST(self):
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        print(self.data_string)
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+
+        set()
+        return
+
+
+def run():
+    print('starting server...')
+
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = ('0.0.0.0', 9003)
+    httpd = HTTPServer(server_address, testHttpServerRequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+
+run()
